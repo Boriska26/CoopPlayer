ffc692d8ab912634f8d1d861d22f07db68170e10
DESKTOP-LF99SUP\dytel <winner1489@gmail.com>
2026-01-09 17:50:01 +0700

    Первый комит

---
+++ CoopPlayer/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace CoopPlayer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+
+}
